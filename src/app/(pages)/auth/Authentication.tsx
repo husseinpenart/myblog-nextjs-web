43cdb150f8ad1703ce7753b6a962ddc2ea0fa8cd
hussein <artistpenman1515@gmail.com>
2025-09-25 15:15:07 +0330

    tabs aadded with animation

--- src/app/(pages)/auth/Authentication.tsx
+++ src/app/(pages)/auth/Authentication.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Tabs from '@/app/components/global/Tabs'
 import React from 'react'
 
--- src/app/components/global/Tabs.tsx
+++ src/app/components/global/Tabs.tsx
@@ -1,24 +1,45 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Login from '../auth/Login'
 import Register from '../auth/Register'
+import { TabStatus } from '../@types/index.global'
+import { motion, AnimatePresence } from "framer-motion";
 
 const Tabs = () => {
+    const [tabs, setTabs] = useState<TabStatus>("Login")
+    const handleChangeTab = (tabs: "Login" | "Register") => {
+        setTabs(tabs)
+    }
     return (
         <div>
             <div className='border-2 border-white  inset-2 shadow-md shadow-amber-100 bg-amber-100 rounded-xl p-2 w-[50rem]'>
-
                 <div className='flex flex-wrap justify-center justify-items-center gap-10 border-b border-gray-300'>
-                    <div> <button className='text-center text-black p-1 '>Login</button></div>
+                    <div> <button className={`${tabs == "Login" ? " font-[NunitoBold] text-xl " : ""} text-center text-black p-1 cursor-pointer`} onClick={() => handleChangeTab("Login")} >Login</button></div>
                     <div className=' border-l h-10 border-gray-600 '></div>
-                    <div> <button className='text-center text-black p-1 '>Register</button></div>
+                    <div> <button className={`${tabs == "Register" ? " font-[NunitoBold] text-xl " : ""} text-center text-black p-1 cursor-pointer`} onClick={() => handleChangeTab("Register")}>Register</button></div>
                 </div>
                 {/* /// login form and registration */}
-                {/* <Login />    */}
-                <Register />
-
+                <AnimatePresence mode='wait'>
+                    {tabs == "Login" ? <div className=''>
+                        <motion.div
+                            key="login"
+                            initial={{ opacity: 0, x: -50 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            exit={{ opacity: 0, x: 50 }}
+                            transition={{ duration: 0.2 }}
+                        >
+                            <Login />
+                        </motion.div>
+                    </div> : <motion.div
+                        key="register"
+                        initial={{ opacity: 0, x: 50 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: -50 }}
+                        transition={{ duration: 0.4 }}
+                    >
+                        <Register />
+                    </motion.div>}
+                </AnimatePresence>
             </div>
-
-
         </div>
     )
 }
